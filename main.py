8a1d87eddbbc3c9d9624f1a01148f6b2bf66da0d
hasan foraty <hasanforaty19@gmail.com>
2024-03-07 13:02:19 +0330

    add logo

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import tkinter as tk
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+windows = tk.Tk()
+windows.title("Password Manager")
+windows.config(padx=20, pady=20)
+
+canvas = tk.Canvas(windows, width=200, height=200)
+logo_img = tk.PhotoImage(file="assets/logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=0, row=0)
+
+
+windows.mainloop()
