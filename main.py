9fc5c4cdd7daf546d68672eab2761c0a43943052
hasan foraty <hasanforaty19@gmail.com>
2024-03-07 16:51:21 +0330

    fix alignment

--- main.py
+++ main.py
@@ -24,17 +24,17 @@ password_label = tk.Label(windows, text="Password:", font=Label_Fonts)
 password_label.grid(column=0, row=3)
 
 # Entry's
-web_entry = tk.Entry(font=Label_Fonts, width=35)
+web_entry = tk.Entry(font=Label_Fonts, width=40)
 web_entry.grid(column=1, row=1, columnspan=2)
-username_entry = tk.Entry(font=Label_Fonts, width=35)
+username_entry = tk.Entry(font=Label_Fonts, width=40)
 username_entry.grid(column=1, row=2, columnspan=2)
-password_entry = tk.Entry(font=Label_Fonts, width=21)
+password_entry = tk.Entry(font=Label_Fonts, width=20)
 password_entry.grid(column=1, row=3)
 
 # Buttons
-generate_button = tk.Button(windows, text="Generate Password", font=Label_Fonts,)
+generate_button = tk.Button(windows, text="Generate Password", font=Label_Fonts)
 generate_button.grid(column=2, row=3)
-add_button = tk.Button(windows, text="Add Password", width=36)
+add_button = tk.Button(windows, text="Add Password", width=30)
 add_button.grid(column=1, row=4, columnspan=2)
 
 windows.mainloop()
