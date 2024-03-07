f32785ed1f43fd1f81bf1d874e209cbc8787cdb7
hasan foraty <hasanforaty19@gmail.com>
2024-03-07 17:25:28 +0330

    add generate password functionality

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 import tkinter as tk
+from tkinter import messagebox as ms
 
 Label_Fonts = ("Arial", 14)
-
+from random import randint, choice, shuffle
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def create_password():
+    letter_list = [choice(letters) for _ in range(randint(8, 10))]
+    symbol_list = [choice(symbols) for _ in range(randint(2, 4))]
+    number_list = [choice(numbers) for _ in range(randint(2, 4))]
+    password_list = letter_list + symbol_list + number_list
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    return password
+
+
+def generate_password_button():
+    password_entry.delete(0, tk.END)
+    password = create_password()
+    password_entry.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_data():
-    with open("data.txt", "a") as file:
-        email = str(username_entry.get())
-        password = str(password_entry.get())
-        website = str(web_entry.get())
-        file.write(f'Website: {website} || Email: {email} || Password: {password}\n')
-        password_entry.delete(0, tk.END)
-        web_entry.delete(0, tk.END)
+    email = str(username_entry.get())
+    password = str(password_entry.get())
+    website = str(web_entry.get())
+    if email == "" or password == "" or website == "":
+        ms.showerror("Error", "Please enter all fields")
+        return
+    is_ok = ms.askyesno(title=website, message=f"This are the details entered : \n Email: {email} \n "
+                                               f"Password: {password}\n is it oky to continue?")
+    if is_ok:
+        with open("data.txt", "a") as file:
+            file.write(f'Website: {website} || Email: {email} || Password: {password}\n')
+            password_entry.delete(0, tk.END)
+            web_entry.delete(0, tk.END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -45,7 +78,7 @@ password_entry = tk.Entry(font=Label_Fonts, width=20)
 password_entry.grid(column=1, row=3)
 
 # Buttons
-generate_button = tk.Button(windows, text="Generate Password", font=Label_Fonts)
+generate_button = tk.Button(windows, text="Generate Password", font=Label_Fonts, command=generate_password_button)
 generate_button.grid(column=2, row=3)
 add_button = tk.Button(windows, text="Add Password", width=30, command=add_data)
 add_button.grid(column=1, row=4, columnspan=2)
